b1216e9e33f16da6a830bebfd09189e3e2bd1f05
Net2Dev <info@a3b.io>
2022-04-22 23:48:24 -0700

    Update Pausable.sol

--- Pausable.sol
+++ Pausable.sol
@@ -2,8 +2,8 @@
 
 pragma solidity 0.8.4;
 
-import "https://github.com/net2devcrypto/n2dstaking/002/Ownable.sol";
-import "https://github.com/net2devcrypto/n2dstaking/002/SafeMath.sol";
+import "https://github.com/net2devcrypto/002/Ownable.sol";
+import "https://github.com/net2devcrypto/002/SafeMath.sol";
 
 contract Pausable is Ownable {
     mapping(uint256 => bool) isPaused;
